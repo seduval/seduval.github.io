c2dbdfb321554caed68496255d602302003e3898
seduval <seduval@morpho.voo.be>
2021-01-17 14:22:09 +0100

    test lang other

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,6 +1,6 @@
 let langs = ['en', 'fr'];
-let lang = 'en';
-setLangStyles(lang);
+// let lang = 'en';
+// setLangStyles(lang);
 
 function setStyles(styles) {
     var elementId = '__lang_styles';
@@ -62,3 +62,84 @@ function setPageTitleLang(lang) {
         page.title = page.title_en;
     }
 }*/
+
+$(function () {
+  ///// Language Switching (2 languages: English and French). /////
+
+  // Initially disable language switching button.
+  $('#switch-lang').css({'pointer-events':'none',
+   'cursor':'default'}).attr('disabled','disabled');
+
+  function langButtonListen() {
+    $('#switch-lang').click(function (event) {
+      event.preventDefault();
+      $('[lang="fr"]').toggle();
+      $('[lang="en"]').toggle();
+      // Switch cookie stored language.
+      if ($.cookie('lang') === 'en') {
+        $.cookie('lang', 'fr', { expires: 7 });
+      } else {
+        $.cookie('lang', 'en', { expires: 7 });
+      }
+    });
+    // Enable lang switching button.
+    $('#switch-lang').css({'pointer-events':'auto',
+     'cursor':'pointer'}).removeAttr('disabled');
+  }
+
+  // Check if language cookie already exists.
+  if ($.cookie('lang')) {
+    var lang = $.cookie('lang');
+    if (lang === 'en') {
+      $('[lang="fr"]').hide();
+      langButtonListen();
+    } else {
+      $('[lang="en"]').hide();
+      langButtonListen();
+    }
+  } else {
+    // no cookie set, so detect language based on location.
+    if ("geolocation" in navigator) {
+      // geolocation is available
+      navigator.geolocation.getCurrentPosition(function (position) {
+        // accepted geolocation so figure out which country
+        var lat = position.coords.latitude,
+            lng = position.coords.longitude;
+        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lng+'&sensor=true', null, function (response) {
+          var country = response.results[response.results.length-1].formatted_address;
+          if (country ===  'France') {
+            $('[lang="en"]').hide();
+            $.cookie('lang', 'fr', { expires: 7 });
+            langButtonListen();
+          } else {
+            $('[lang="fr"]').hide();
+            $.cookie('lang', 'en', { expires: 7 });
+            langButtonListen();
+          }
+        }).fail(function (err) {
+          console.log('error: '+err);
+          $('[lang="fr"]').hide();
+          $.cookie('lang', 'en', { expires: 7 });
+          langButtonListen();
+        });
+      },
+      function (error) {
+        if (error.code == error.PERMISSION_DENIED) {
+          // denied geolocation
+          $('[lang="fr"]').hide();
+          $.cookie('lang', 'en', { expires: 7 });
+          langButtonListen();
+        } else {
+          console.log('Unknown error. Defaulting to English!');
+          $('[lang="fr"]').hide();
+          $.cookie('lang', 'en', { expires: 7 });
+          langButtonListen();
+        }
+      });
+    } else {
+      // geolocation IS NOT available
+      $('[lang="fr"]').hide();
+      $.cookie('lang', 'en', { expires: 7 });
+      langButtonListen());
+    }
+  }
