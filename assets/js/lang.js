6f10f04fb8a6cc9719105c060295e42ddbf771d0
seduval <seduval@morpho.voo.be>
2021-01-17 14:26:13 +0100

    test lang other

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,67 +1,67 @@
-let langs = ['en', 'fr'];
+// let langs = ['en', 'fr'];
 // let lang = 'en';
 // setLangStyles(lang);
-
-function setStyles(styles) {
-    var elementId = '__lang_styles';
-    var element = document.getElementById(elementId);
-    if (element) {
-        element.remove();
-    }
-
-    let style = document.createElement('style');
-    style.id = elementId;
-    style.type = 'text/css';
-
-    if (style.styleSheet) {
-        style.styleSheet.cssText = styles;
-    } else {
-        style.appendChild(document.createTextNode(styles));
-    }
-    document.getElementsByTagName('head')[0].appendChild(style);
-}
-
-function switchLang() {
-    if (lang == 'en') {
-        setLang('fr');
-        lang = 'fr';
-        document.getElementById("lang-toggle").checked = true;
-    }
-    else {
-        setLang('en');
-        lang = 'en';
-        document.getElementById("lang-toggle").checked = false;
-    }
-    document.referrer.hreflang = lang;
-}
-
-function setLang(lang) {
-    setLangStyles(lang);
-}
-
-function setLangStyles(lang) {
-    let styles = langs
-    .filter(function (l) {
-        return l != lang;
-    })
-    .map(function (l) {
-        return ':lang('+ l +') { display: none; }';
-    })
-    .join(' ');
-
-    setStyles(styles);
-    
-//     setPageTitleLang(lang)
-}
-/*
-function setPageTitleLang(lang) {
-    if (lang=='fr'){
-        page.title = page.title_fr;
-    }
-    else {
-        page.title = page.title_en;
-    }
-}*/
+// 
+// function setStyles(styles) {
+//     var elementId = '__lang_styles';
+//     var element = document.getElementById(elementId);
+//     if (element) {
+//         element.remove();
+//     }
+// 
+//     let style = document.createElement('style');
+//     style.id = elementId;
+//     style.type = 'text/css';
+// 
+//     if (style.styleSheet) {
+//         style.styleSheet.cssText = styles;
+//     } else {
+//         style.appendChild(document.createTextNode(styles));
+//     }
+//     document.getElementsByTagName('head')[0].appendChild(style);
+// }
+// 
+// function switchLang() {
+//     if (lang == 'en') {
+//         setLang('fr');
+//         lang = 'fr';
+//         document.getElementById("lang-toggle").checked = true;
+//     }
+//     else {
+//         setLang('en');
+//         lang = 'en';
+//         document.getElementById("lang-toggle").checked = false;
+//     }
+//     document.referrer.hreflang = lang;
+// }
+// 
+// function setLang(lang) {
+//     setLangStyles(lang);
+// }
+// 
+// function setLangStyles(lang) {
+//     let styles = langs
+//     .filter(function (l) {
+//         return l != lang;
+//     })
+//     .map(function (l) {
+//         return ':lang('+ l +') { display: none; }';
+//     })
+//     .join(' ');
+// 
+//     setStyles(styles);
+//     
+// //     setPageTitleLang(lang)
+// }
+// /*
+// function setPageTitleLang(lang) {
+//     if (lang=='fr'){
+//         page.title = page.title_fr;
+//     }
+//     else {
+//         page.title = page.title_en;
+//     }
+// }*/
 
 $(function () {
   ///// Language Switching (2 languages: English and French). /////
